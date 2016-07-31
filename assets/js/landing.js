a39e6a9c752c00a485daec382892f0d9d214ec28
Grompokstar <roma.chervontsev@gmail.com>
2016-07-31 20:59:18 +0300

    доработки анимации тарифов

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,11 +1,26 @@
 $(function(){
 
     var tariffs =  $('.tariff-block');
+    var functionality =  $('.tariff-functionality');
     tariffs.addClass('hidden');
+    tariffs.addClass('hidden-left');
+
+    functionality.addClass('hidden');
+    functionality.addClass('visuallyhidden');
 
     var activeTariffId = $('.tariffs li.active').attr('data-tariff-id');
     var activeTariff = $('#tariff-' + activeTariffId);
-        activeTariff.removeClass('hidden');
+
+    activeTariff.removeClass('hidden');
+    setTimeout(function () {
+        activeTariff.removeClass('hidden-left');
+    }, 20);
+
+    var activeFunctionality = $('#functionality-' + activeTariffId);
+    activeFunctionality.removeClass('hidden');
+    setTimeout(function () {
+        activeFunctionality.removeClass('visuallyhidden');
+    }, 20);
 
     var tariffsNav = $('.tariffs li');
 
@@ -14,19 +29,28 @@ $(function(){
         $(this).addClass('active');
 
         var tariffId = $(this).attr('data-tariff-id'),
-            targetTariff = $('#tariff-' + tariffId);
+            targetTariff = $('#tariff-' + tariffId),
+            targetFunctionality = $('#functionality-' + tariffId);
 
         tariffs.addClass('hidden');
+        tariffs.addClass('hidden-left');
+
+        functionality.addClass('hidden');
+        functionality.addClass('visuallyhidden');
 
         if (targetTariff.hasClass('hidden')) {
             targetTariff.removeClass('hidden');
             setTimeout(function () {
-                targetTariff.removeClass('visuallyhidden');
+                targetTariff.removeClass('hidden-left');
             }, 20);
-        } else {
-
         }
 
+        if (targetFunctionality.hasClass('hidden')) {
+            targetFunctionality.removeClass('hidden');
+            setTimeout(function () {
+                targetFunctionality.removeClass('visuallyhidden');
+            }, 20);
+        }
     })
 
 });
