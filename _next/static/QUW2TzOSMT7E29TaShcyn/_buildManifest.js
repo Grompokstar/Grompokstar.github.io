83fa6955865ba224de61d6c5b0221546286629f0
Roman Chervontsev <roma.chervontsev@gmail.com>
2022-10-02 19:08:34 +0300

    smecs

---
+++ _next/static/QUW2TzOSMT7E29TaShcyn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd299649c9dc034c.css","static/chunks/pages/index-7b0eb4a522f5f3a3.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
